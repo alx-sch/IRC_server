9ed7014dd53731657d2d6715780217a16f872c5f
pvasilan <pvasilan@student.42berlin.de>
2025-07-16 13:26:33 +0200

    beginning client class and also adding 2 maps in server

---
+++ include/Client.hpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Client
+{
+private:
+	int fd;
+	std::string nickname;
+	std::string username;
+public:
+	Client(/* args */);
+	~Client();
+};
+
+Client::Client(/* args */)
+{
+}
+
+Client::~Client()
+{
+}
+
+
--- include/Server.hpp
+++ include/Server.hpp
@@ -1,4 +1,26 @@
+#include <map>
+#include "Client.hpp"
+
 #ifndef SERVER_HPP
 # define SERVER_HPP
+
+class Server
+{
+private:
+	std::map<int, Client*> fd_client_map;
+	std::map<std::string, Client*> nickname_client_map;
+public:
+	Server(/* args */);
+	~Server();
+};
+
+Server::Server(/* args */)
+{
+}
+
+Server::~Server()
+{
+}
+
 
 # endif
