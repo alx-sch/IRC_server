6cb5c52186265ea150fd828fddde35e3d23cbbf9
pvasilan <pvasilan@student.42berlin.de>
2025-07-18 15:59:14 +0200

    fleshing out the channel class

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -6,8 +6,17 @@
 class Channel
 {
 private:
+	std::string channel_name;
+	std::string channel_topic;
 	std::set<std::string> channel_members_by_nickname;
 	std::set<std::string> channel_operators_by_nickname;
+
+	// MODE RELATED VARS
+	int user_limit; // set by l
+	bool invite_only; // set by i
+	bool topic_protection; // set by t
+	std::string channel_key; // password set by k
+
 public:
 	Channel(/* args */);
 	~Channel();
