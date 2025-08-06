500708d7e27f102b98b4c9299c986350f52d4b0e
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2025-08-06 13:33:44 +0000

    fix newline issue (see above) also for NOTICE

--- src/CommandMessaging.cpp
+++ src/CommandMessaging.cpp
@@ -175,7 +175,7 @@ static void	handleNoticeToUser(Server* server, User* sender, const std::string& 
 		return;
 	}
 
-	std::string	line = ":" + sender->buildPrefix() + " NOTICE " + targetNick + " :" + message;
+	std::string	line = ":" + sender->buildPrefix() + " NOTICE " + targetNick + " :" + message + "\n";
 	targetUser->getOutputBuffer() += line;
 
 	logUserAction(sender->getNickname(), sender->getFd(),
